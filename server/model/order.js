a57ee07273168eeca933ce5d36c3a92696ad6b7f
Tochine <toshineluv99@yahoo.com>
2018-05-04 11:06:01 +0100

    [#156839698] add post menu

---
+++ server/model/order.js
@@ -0,0 +1,31 @@
+
+const orders = [
+  {
+  	id: 1,
+  	mealId: 1,
+  	quantity: 2,
+  	contact: '099877789',
+  	time: 9.30,
+  	total: 4000,
+  },
+
+  {
+  	id: 2,
+  	mealId: 1,
+  	quantity: 2,
+  	contact: 99877789,
+  	time: 9.30,
+  	total: 4000,
+  },
+
+  {
+  	id: 3,
+  	mealId: 1,
+  	quantity: 2,
+  	contact: 99877789,
+  	time: 9.30,
+  	total: 4000,
+  }
+];
+
+export default orders;
--- server/route/index.js
+++ server/route/index.js
@@ -1,16 +1,19 @@
 
 
 import mealController from '../controller/meal';
+import orderController from '../controller/order';
 
 
 module.exports = (app) => {
-	app.get('/api', (req, res) => res.status(200).send({
+	app.get('/', (req, res) => res.status(200).send({
 		message: 'Welcome to book-a-meal Api',
 	}));
 
-
+	console.log('orders')
 	app.get('/meals', mealController.getMeal);
 	app.post('/meals', mealController.createMeal);
 	app.put('/meals/:mealId', mealController.updateMeal);
 	app.delete('/meals/:mealId', mealController.deleteMeal);
-} 
+    // app.post('/orders', orderController.createOrder);
+    app.post('/orders', orderController.createOrder);
+}
